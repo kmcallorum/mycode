424c07c050837fce432a01c4ecfb8d7a8313cff4
Kevin McAllorum <kmcallorum@users.noreply.github.com>
2022-08-22 18:28:15 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,19 @@
+package main
+  
+import "fmt"
+
+
+var name, city, state, zip string
+
+func main() {
+    name="Kevin"
+    city="Easley"
+    state="SC"
+    zip="29642"
+
+    fmt.Println(name)
+    fmt.Println(city)
+    fmt.Println(state)
+    fmt.Println(zip)
+}
+
